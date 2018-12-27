bf29ce434e82f6b90f8b233a1d400ad9e1af4ddb
unknown <zhouyanping@vcredit.cn>
2018-12-27 17:40:46 +0800

    更新发邮件功能的附件bug

--- run_all.py
+++ run_all.py
@@ -5,7 +5,26 @@ from email.mime.multipart import MIMEMultipart
 import smtplib
 import os,sys
 
+def find_case():
+    #方法一，找到用例的路径
+    casePath = "F:\study\web_auto\case"
+    rule = "test*.py"
+    discover = unittest.defaultTestLoader.discover(start_dir=casePath,pattern=rule)
+    return discover
+
+def run_case(discover):
+    #方法二，执行用例
+    reportPath = "F:\\study\\web_auto\\report\\"+"result.html"
+    fp = open(reportPath,'wb')
+    runner = HTMLTestRunner_cn.HTMLTestRunner(stream=fp,
+                                              title='报告的title',
+                                              description="描述")
+
+    runner.run(discover)
+    fp.close()
+
 def send_mail(sender,psw,receiver,smtpserver,report_file,port):
+    #方法三，以邮件发送最新的测试报告
     with open(report_file,"rb") as f:
         mail_body = f.read()
     #定义邮件内容
@@ -21,7 +40,7 @@ def send_mail(sender,psw,receiver,smtpserver,report_file,port):
     #添加附件
     att = MIMEText(open(report_file,"rb").read(),"base64","utf-8")
     att["Content-Type"] = "application/octet-stream"
-    att["Content-Disposotion"] = 'attachment; filename="result.html"'
+    att["Content-Disposition"] = 'attachment; filename="report.html"'
     msg.attach(att)
     try:
         smtp = smtplib.SMTP()
@@ -34,23 +53,11 @@ def send_mail(sender,psw,receiver,smtpserver,report_file,port):
     smtp.quit()
 
 if __name__ == "__main__":
-    #用例的路径
-    casePath = "F:\study\web_auto\case"
-    rule = "test*.py"
-    discover = unittest.defaultTestLoader.discover(start_dir=casePath,pattern=rule)
-
-    reportPath = "F:\\study\\web_auto\\report\\"+"result.html"
-    fp = open(reportPath,'wb')
-    runner = HTMLTestRunner_cn.HTMLTestRunner(stream=fp,
-                                              title='报告的title',
-                                              description="描述")
-
-    runner.run(discover)
-    fp.close()
-    propath = os.path.dirname(os.getcwd())
-    print(propath)
-    # file_path = os.path.join(propath, "report","result.html")
-    file_path = "F:\\study\\web_auto\\report\\"+"result.html"
+    find_case = find_case()
+    run_case(find_case)
+    propath = os.path.abspath(os.curdir)
+    file_path = os.path.join(propath, "report","result.html")
+    # file_path = "F:\\study\\web_auto\\report\\"+"result.html"
     sender = "zyp272458867@163.com"
     psw = "abc19910515"
     smtp_server = "smtp.163.com"
